80a5eff7b877926093645114a29a35c0ed5360a4
TheMole3 <hugomelo04@gmail.com>
2021-01-11 10:50:22 +0100

    Clean up and commenting

--- commands/namn.js
+++ commands/namn.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args, fs, config) => {
-  message.delete()
-  let rMember = message.guild.member(message.mentions.users.array()[1])
-  if(args.slice(1).join(" ").length < 32) {
-    rMember.setNickname(args.slice(1).join(" "))
+  message.delete(); // Delete the invokation
+  let rMember = message.guild.member(message.mentions.users.array()[1]); // Get the first mentioned user
+  if(args.slice(1).join(" ").length < 32) { // If the first argument is shorter than 32 chars
+    rMember.setNickname(args.slice(1).join(" ")); // Set the mentioned user nickname to the provided one
   }
 }
